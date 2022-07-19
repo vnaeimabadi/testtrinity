8eee7e1ea18af06b5766e0d5d8eb4caf62b45d4f
vnaeimabadi <vnaeimabadi@vnaeimabadis-MacBook-Pro.local>
2022-07-19 13:39:43 +0430

    end commit

--- src/screens/page-two/PageTwo.js
+++ src/screens/page-two/PageTwo.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {View, Text, TextInput, ScrollView, Keyboard} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {Avatar} from '../../components/atoms/Avatar';
